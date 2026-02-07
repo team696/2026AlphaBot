f21a5c03a18eb846b807aafa7ac68bf388e31c61
pro_ <hahnbin040110@gmail.com>
2026-02-06 17:22:30 -0800

    git is crashing out at me

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,11 @@
 
 package frc.robot;
 
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.pathplanner.lib.auto.AutoBuilder;
 
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -11,13 +16,25 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystem.Swerve;
 import frc.robot.util.Auto;
+import frc.robot.util.constants.DriveConstants;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private final RobotContainer m_robotContainer;
+
+  private final CommandXboxController joystick = new CommandXboxController(0);
+
+  public final SwerveRequest.FieldCentricFacingAngle FCFARequest = 
+    new SwerveRequest.FieldCentricFacingAngle()
+        .withDeadband(DriveConstants.MaxSpeed* .01)
+        .withRotationalDeadband(DriveConstants.MaxAngularRate * .01) // Add a  deadband
+        .withDriveRequestType(DriveRequestType.OpenLoopVoltage)
+        .withHeadingPID(5., 0, 0).withMaxAbsRotationalRate(DegreesPerSecond.of(360))
+        .withRotationalDeadband(DegreesPerSecond.of(1)); 
 
   
 
@@ -32,7 +49,12 @@ public class Robot extends TimedRobot {
             new Auto.NamedCommand("Pass_1", Swerve.get().fakeCommand()),
             new Auto.NamedCommand("Intake", Swerve.get().fakeCommand()),
             new Auto.NamedCommand("Reset Intake", Swerve.get().fakeCommand()),
-            new Auto.NamedCommand("Climb L1", Swerve.get().fakeCommand())
+            new Auto.NamedCommand("Climb L1", Swerve.get().fakeCommand()),
+            new Auto.NamedCommand("Auto Align", Swerve.get().applyRequest(()->
+                    FCFARequest.withVelocityX((-joystick.getLeftY() / 2) * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond)) // Drive forward with negative Y (forward)
+                    .withVelocityY((-joystick.getLeftX() / 2) * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond)) // Drive left with negative X (left)
+                    .withTargetDirection(Swerve.get().hub_target_Theta())))
+            
         );
 
   }
